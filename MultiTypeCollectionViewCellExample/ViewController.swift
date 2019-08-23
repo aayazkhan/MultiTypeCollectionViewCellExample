ff059966a355ab6bc580720eb8306c0289324337
aayazkhan <khan.shan803@gmail.com>
2019-08-23 21:39:14 +0530

    Initial Commit

---
+++ MultiTypeCollectionViewCellExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiTypeCollectionViewCellExample
+//
+//  Created by aayaz Khan on 23/08/19.
+//  Copyright © 2019 aayaz Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
